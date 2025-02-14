026b8bda5a123a977637cbd5f5654f3f888cec5c
svichkar <2012kostyan@gmail.com>
2025-02-14 13:34:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import base64
 
 # Set page config
-st.set_page_config(page_title="Valhalla Countdown", layout="wide", page_icon="favicon.ico")
+st.set_page_config(page_title="Valhalla", layout="wide", page_icon="favicon.ico")
 
 def get_base64(file):
     with open(file, "rb") as f:
